d8870d5582cb426075eb0fbae6d9ca0ae8132eb9
sevvaldnz <129736734+sevvaldnz@users.noreply.github.com>
2023-05-20 22:39:08 +0300

    fifth commit

--- app/src/main/java/com/example/cryptoapp/FindfriendActivity.java
+++ app/src/main/java/com/example/cryptoapp/FindfriendActivity.java
@@ -2,108 +2,106 @@ package com.example.cryptoapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import Model.Users;
-
 
 public class FindfriendActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-    private RecyclerView.Recycler Findfriend_recyler_list;
+    private RecyclerView Findfriend_recycler_list;
 
     private DatabaseReference kullanıcıyolu;
-    @Override
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_findfriend);
 
-        //recyler
-        Findfriend_recyler_list=findViewById(R.id.Findfriend_recyler_list);
-        Findfriend_recyler_list.setViewCacheSize(new LinearLayoutManager(context list));
+        // RecyclerView
+        Findfriend_recycler_list = findViewById(R.id.findfriend_recyler_list);
+        Findfriend_recycler_list.setLayoutManager(new LinearLayoutManager(this));
 
-
-        //Toolbar
-        mToolbar=findViewById(R.id.findfriend_toolbar);
+        // Toolbar
+        mToolbar = findViewById(R.id.findfriend_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("Find Friend");
+        getSupportActionBar().setTitle("Choose Contact");
 
-        kullanıcıyolu= FirebaseDatabase.getInstance().getReference().child(Users);
-
+        kullanıcıyolu = FirebaseDatabase.getInstance().getReference().child("Users");
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        // in start
 
-        //sorgu
+        // Query
+        Query query = kullanıcıyolu;
 
-        FirebaseOptions.<Users> seçenekler=new FirebaseOptions.Builder<Users>().setQuery (kullanıcıyolu Users.class)
-                .build();
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(query, Users.class)
+                        .build();
 
+        FirebaseRecyclerAdapter<Users, FindfriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Users, FindfriendViewHolder>(options) {
+                    @NonNull
+                    @Override
+                    public FindfriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.show_user_layout, viewGroup, false);
+                        return new FindfriendViewHolder(view);
+                    }
 
-        Object seçenekler;
-        activityFirebaseRecyclerAdapter<Users, FindfriendActivity>adapter = new activityFirebaseRecyclerAdapte <users, FindfriendActivity>(seçenekler){
-        @Override
-        protected void onBindViewHolder(@NonNull FindfriendActivity, int position, @NonNull Users)
+                    @SuppressLint("RecyclerView")
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindfriendViewHolder holder, int position, @NonNull Users model) {
+                        holder.username.setText(model.getUsername());
+                        holder.about.setText(model.getAbout());
 
-            holder.username.setText(model.getusername);
-            holder.about.setText(model.getabout);
+                        // Click Listener
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
 
+                                String tıklanan_kullanıcı_Id_göster = getRef(position).getKey();
+                                // Perform your action here
+                            }
+                        });
+                    }
+                };
 
-            //Tıklandığında
-            holder.itemView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View){
-                    String tıklanan_kullanıcı_Id_göster= getReferrer(position).getKey()
-                }
-            }
-
-        };
-
-        {
-            @NonNull
-            @Override
-            public FindfriendActivity onCreateViewHolder(ViewGroup viewGroup, int i){
-
-               View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.show_user_layout, viewGroup, false);
-                FindfriendActivity viewHolder = new FindfriendActivity(view);
-                return viewHolder;
-        }
-        }    } ;
-
-
-    FindfriendActivity.setAdapter(adapter);
-    adapter.notifyDataSetChanged();
-    adapter.startListening;
-
-       public static class FindfriendViewHolder extends  RecyclerView.ViewHolder{
-
-           TextView username, about;
-
-
+        Findfriend_recycler_list.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        adapter.startListening();
     }
-     public static FindfriendActivity(@NonNull View itemView){
-        super(itemView);
 
-        //Tanımlar
-        username=itemView.findViewById(R.id.usernameEditText);
-        about=itemView.findViewById(R.id.aboutEditText);
-     }
+    public static class FindfriendViewHolder extends RecyclerView.ViewHolder {
 
+        TextView username, about;
+
+        public FindfriendViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Assignments
+            username = itemView.findViewById(R.id.usernameEditText);
+            about = itemView.findViewById(R.id.aboutEditText);
+        }
+    }
 }
